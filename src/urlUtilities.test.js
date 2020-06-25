2fa88532f3379205d50e2447d404fa705c283e1b
Michael Rarela <michael.rarela@dollarshaveclub.com>
2020-06-24 17:02:01 -0700

    chore: add isSameOriginAsCurrentPage()

--- src/urlUtilities.test.js
+++ src/urlUtilities.test.js
@@ -1,4 +1,4 @@
-import { parseUrl } from './urlUtilities'
+import { parseUrl, isSameOriginAsCurrentPage } from './urlUtilities'
 
 describe('parseUrl()', () => {
   describe('parsing relative urls', () => {
@@ -27,5 +27,58 @@ describe('parseUrl()', () => {
       const result = parseUrl(FULL_URL)
       expect(result.origin).toBe('http://www.shave.com')
     })
+  })
+})
+
+describe('isSameOriginAsCurrentPage()', () => {
+  const MOCK_LOCATION = {
+    hash: '',
+    host: 'uk.dollarshaveclub.com',
+    hostname: 'uk.dollarshaveclub.com',
+    href: 'https://uk.dollarshaveclub.com/',
+    origin: 'https://uk.dollarshaveclub.com',
+    pathname: '/',
+    port: '',
+    protocol: 'https:',
+  }
+
+  let realLocation
+
+  beforeAll(() => {
+    realLocation = window.location
+    delete window.location
+    window.location = MOCK_LOCATION
+  })
+
+  afterAll(() => {
+    window.location = realLocation
+  })
+
+  it('META: mocks correctly', () => {
+    expect(window.location.host).toEqual('uk.dollarshaveclub.com')
+  })
+
+  it('retuns true, when origins match', () => {
+    const result = isSameOriginAsCurrentPage(
+      'https://uk.dollarshaveclub.com/get-started'
+    )
+    expect(result).toBe(true)
+  })
+
+  it('returns false, when origins do not match (V1)', () => {
+    const result = isSameOriginAsCurrentPage(
+      'https://ca.dollarshaveclub.com/get-started'
+    )
+    expect(result).toBe(false)
+  })
+
+  it('returns false, when origins do not match (V2)', () => {
+    const result = isSameOriginAsCurrentPage('http://www.google.com')
+    expect(result).toBe(false)
+  })
+
+  it('returns true, when relative url sent', () => {
+    const result = isSameOriginAsCurrentPage('/get-started')
+    expect(result).toBe(true)
   })
 })
